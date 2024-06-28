54bd529fcc57a944450e3c163ebdf53d277dfc1a
Alex254715216775 <144353929+Alex254715216775@users.noreply.github.com>
2024-06-28 04:38:46 +0300

    Add files via upload

---
+++ RadioButtonDemo.java
@@ -0,0 +1,124 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class RadioButtonDemo extends JFrame 
+{
+    private JRadioButton birdButton;
+    private JRadioButton catButton;
+    private JRadioButton dogButton;
+    private JRadioButton rabbitButton;
+    private JRadioButton pigButton;
+    private JLabel petImageLabel;
+
+    public RadioButtonDemo() 
+    {
+        createView();
+
+        setTitle("RadioButtonDemo");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600, 600);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    private void createView() 
+    {
+        JPanel panel = new JPanel();
+        getContentPane().add(panel);
+
+        panel.setLayout(new GridLayout(2, 1));
+
+        JPanel radioPanel = new JPanel();
+        panel.add(radioPanel);
+
+  
+        radioPanel.setLayout(new BoxLayout(radioPanel, BoxLayout.Y_AXIS));
+
+        birdButton = new JRadioButton("Bird");
+        catButton = new JRadioButton("Cat");
+        dogButton = new JRadioButton("Dog");
+        rabbitButton = new JRadioButton("Rabbit");
+        pigButton = new JRadioButton("Pig");
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(birdButton);
+        group.add(catButton);
+        group.add(dogButton);
+        group.add(rabbitButton);
+        group.add(pigButton);
+
+        radioPanel.add(birdButton);
+        radioPanel.add(catButton);
+        radioPanel.add(dogButton);
+        radioPanel.add(rabbitButton);
+        radioPanel.add(pigButton);
+
+        radioPanel.add(Box.createVerticalGlue());
+
+        birdButton.addActionListener(new PetButtonListener());
+        catButton.addActionListener(new PetButtonListener());
+        dogButton.addActionListener(new PetButtonListener());
+        rabbitButton.addActionListener(new PetButtonListener());
+        pigButton.addActionListener(new PetButtonListener());
+
+        petImageLabel = new JLabel();
+        panel.add(petImageLabel);
+
+        pigButton.setSelected(true);
+        displayImage("Pig.jpeg");
+    }
+
+    private void displayImage(String imageName) 
+    {
+        URL imgURL = getClass().getClassLoader().getResource("images/" + imageName);
+        if (imgURL != null) 
+        {
+            ImageIcon icon = new ImageIcon(imgURL);
+            petImageLabel.setIcon(icon);
+        } else 
+        {
+            System.err.println("Couldn't find file: " + imageName);
+        }
+    }
+
+    private class PetButtonListener implements ActionListener 
+    {
+        @Override
+        public void actionPerformed(ActionEvent e) 
+        {
+            JRadioButton source = (JRadioButton) e.getSource();
+            String imageName = "images/";
+
+            if (source == birdButton) 
+            {
+                imageName = "Bird.jpeg";
+            } else if (source == catButton) 
+            {
+                imageName = "Cat.jpeg";
+            } else if (source == dogButton) 
+            {
+                imageName = "Dog.jpeg";
+            } else if (source == rabbitButton) 
+            {
+                imageName = "Rabbit.jpeg";
+            } else if (source == pigButton) 
+            {
+                imageName = "Pig.jpeg";
+            }
+
+            displayImage(imageName);
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        SwingUtilities.invokeLater(() -> 
+        {
+            new RadioButtonDemo().setVisible(true);
+        });
+    }
+}
